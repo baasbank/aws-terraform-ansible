026159e6c18a09105164dca22b43b9d8cc44ca2b
Baasbank <baasb4real@yahoo.com>
2019-09-14 21:27:00 +0100

    create RDS instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -180,7 +180,7 @@ resource "aws_subnet" "wp_rds3_subnet" {
 
 # RDS Subnet Group
 
-resource "aws_db_subnet_group" "wp_rds_subnet" {
+resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
   name = "wp_rds_subnetgroup"
 
   subnet_ids = ["${aws_subnet.wp_rds1_subnet.id}",
@@ -353,4 +353,19 @@ resource "aws_s3_bucket" "wp_code" {
   tags = {
     Name = "code bucket"
   }
+}
+
+# ********** RDS **********
+
+resource "aws_db_instance" "wp_db" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.6.27"
+  instance_class         = "${var.db_instance_class}"
+  name                   = "${var.dbname}"
+  username               = "${var.dbuser}"
+  password               = "${var.dbpassword}"
+  db_subnet_group_name   = "${aws_db_subnet_group.wp_rds_subnetgroup.name}"
+  vpc_security_group_ids = ["${aws_security_group.wp_rds_sg.id}"]
+  skip_final_snapshot    = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,15 @@ variable "aws_profile" {}
 variable "vpc_cidr" {}
 variable "localip" {}
 variable "domain_name" {}
+variable "db_instance_class" {}
+variable "dbname" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+variable dev_instance_type {}
+variable dev_ami {}
+variable key_name {}
+variable public_key_path {}
+
 
 variable "cidrs" {
   type = "map"
