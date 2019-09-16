c54fc8cc48f84291151f409827326833b69d7727
Baasbank <baasb4real@yahoo.com>
2019-09-16 18:32:44 +0100

    create autoscaling group

--- terraform/main.tf
+++ terraform/main.tf
@@ -493,3 +493,31 @@ resource "aws_launch_configuration" "wp_launch_configuration" {
   }
 }
 
+# ********** AUTO SCALING GROUP **********
+
+resource "aws_autoscaling_group" "wp_asg" {
+  name = "asg-${aws_launch_configuration.wp_launch_configuration.id}"
+  max_size = var.asg_max
+  min_size = var.asg_min
+  health_check_grace_period = var.asg_grace
+  health_check_type = var.asg_health_check_type
+  desired_capacity = var.asg_desired_capacity
+  force_delete = true
+  load_balancers = [aws_elb.wp_elb.id]
+
+  vpc_zone_identifier = [aws_subnet.wp_private1_subnet.id,
+    aws_subnet.wp_private2_subnet.id
+  ]
+
+  launch_configuration = aws_launch_configuration.wp_launch_configuration.name
+
+  tag {
+    key = "Name"
+    value = "wp_asg-instance"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
